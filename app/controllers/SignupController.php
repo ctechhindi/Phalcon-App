ed90f29cb509dd64516b0fd29e3cb0fc51eaf7ac
jeevan15498 <jeevan15498@gmail.com>
2018-04-27 11:00:21 -0700

    - Designing a sign-up form - Database Connection and Storing data using models

---
+++ app/controllers/SignupController.php
@@ -0,0 +1,38 @@
+<?php
+
+class SignupController extends ControllerBase
+{
+
+    public function indexAction()
+    {
+    }
+
+    public function registerAction()
+    {
+        $user = new Users();
+
+        // Store and check for errors
+        $success = $user->save(
+            $this->request->getPost(),
+            [
+                "name",
+                "email",
+            ]
+        );
+
+        if ($success) {
+            echo "Thanks for registering!";
+        } else {
+            echo "Sorry, the following problems were generated: ";
+
+            $messages = $user->getMessages();
+
+            foreach ($messages as $message) {
+                echo $message->getMessage(), "<br/>";
+            }
+        }
+
+        $this->view->disable();
+    }
+}
+
